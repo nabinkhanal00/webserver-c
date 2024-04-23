165c970f31b6cd2554df50ad221ce3c7e0fb64d3
Nabin Khanal <nabinkhanal@skiff.com>
2024-04-24 01:21:41 +0545

    add multi threaded capability

--- server.c
+++ server.c
@@ -1,16 +1,16 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "common.h"
 
+void *handle_connection(void *);
 // TODO: make it multithreaded
 int main() {
     int listenfd, connfd, n;
     struct sockaddr_in serveaddr;
-
-    uint8_t buff[MAXLINE];
-    uint8_t recvline[MAXLINE];
 
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         err_n_die("error while creating a socket");
@@ -43,22 +43,34 @@ int main() {
         inet_ntop(AF_INET, &addr, address, MAXLINE);
         fprintf(stdout, "Connected to: %s\n", address);
 
-        memset(recvline, 0, sizeof(recvline));
-
-        while ((n = read(connfd, recvline, MAXLINE)) > 0) {
-            const char *data = bin2hex(recvline, n);
-            fprintf(stdout, "\n%s\n\n%s", data, recvline);
-            free((void *)data);
-            if (recvline[n - 1] == '\n') {
-                break;
-            }
-            memset(recvline, 0, sizeof(recvline));
-        }
-        if (n < 0) {
-            err_n_die("read error");
-        }
-        snprintf((char *)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello");
-        write(connfd, (char *)buff, strlen((char *)buff));
-        close(connfd);
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_connection, (void *)&connfd);
     }
+}
+
+void *handle_connection(void *args) {
+    int connfd = *((int *)args);
+    uint8_t buff[MAXLINE];
+    uint8_t recvline[MAXLINE];
+    int n;
+
+    memset(recvline, 0, sizeof(recvline));
+
+    while ((n = read(connfd, recvline, MAXLINE)) > 0) {
+        const char *data = bin2hex(recvline, n);
+        fprintf(stdout, "\n%s\n\n%s", data, recvline);
+        free((void *)data);
+        if (recvline[n - 1] == '\n') {
+            break;
+        }
+        memset(recvline, 0, sizeof(recvline));
+    }
+    if (n < 0) {
+        err_n_die("read error");
+    }
+    snprintf((char *)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello");
+    sleep(1);
+    write(connfd, (char *)buff, strlen((char *)buff));
+    close(connfd);
+    return NULL;
 }
